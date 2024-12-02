af130476602a4aa1558f127af4cea8dba1383d5f
Rakibul-Hasan270 <rakibx270@gmail.com>
2024-12-02 09:00:26 +0600

    added one time showing on ui in handelWantToCook others give a toast

--- src/components/Recipes/Recipes.jsx
+++ src/components/Recipes/Recipes.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
-import { useEffect } from "react";
 import Recipe from "../Recipe/Recipe";
+import { useState, useEffect } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Recipes = () => {
     const [recipes, setRecipes] = useState([]);
     const [handelRecipes, setHandelRecipes] = useState([]);
+    const [handelPreparing, setHandelPreparing] = useState([]);
 
     useEffect(() => {
         fetch('recipe.json')
@@ -13,60 +15,94 @@ const Recipes = () => {
     }, []);
 
     const handelWantToCook = recipe => {
-        setHandelRecipes([...handelRecipes, recipe]);
-    }
-    console.log(handelRecipes)
+        const isRecipeAlreadyAdded = handelRecipes.some(r => r.recipe_name === recipe.recipe_name);
+
+        if (isRecipeAlreadyAdded) {
+            toast("This recipe is already in your Want to Cook list.");
+        } else {
+            setHandelRecipes([...handelRecipes, recipe]);
+        }
+    };
+
+    const handelPreparingBtn = info => {
+        setHandelPreparing([...handelPreparing, info]);
+    };
+
     return (
         <div className="">
             <h3 className="text-5xl font-bold text-center">Our Recipes</h3>
-            <p className="mt-5 mb-6 text-xl text-center md:w-3/4 mx-auto">Lorem ipsum dolor sit amet consectetur. Proin et feugiat senectus vulputate netus pharetra rhoncus. Eget urna volutpat curabitur elementum mauris aenean neque. </p>
+            <p className="mt-5 mb-6 text-xl text-center md:w-3/4 mx-auto">
+                Lorem ipsum dolor sit amet consectetur. Proin et feugiat senectus vulputate netus pharetra rhoncus.
+                Eget urna volutpat curabitur elementum mauris aenean neque.
+            </p>
 
             <div className="md:flex gap-10 mt-6">
                 <div className="md:flex gap-4">
                     {
-                        recipes.map((recipe, idx) => <Recipe key={idx} recipe={recipe} handelWantToCook={handelWantToCook}></Recipe>)
+                        recipes.map((recipe, idx) =>
+                            <Recipe
+                                key={idx}
+                                recipe={recipe}
+                                handelWantToCook={handelWantToCook}
+                            />
+                        )
                     }
                 </div>
                 <div className="border md:p-7 rounded-2xl">
-                    <h3 className="border-b-2 pb-3 text-3xl font-bold text-center">Want To Cook: {handelRecipes.length}</h3>
+                    <h3 className="border-b-2 pb-3 text-3xl font-bold text-center">
+                        Want To Cook: {handelRecipes.length}
+                    </h3>
                     <div>
-
                         <div className="overflow-x-auto">
                             <table className="table">
-                                <thead className="text-xl">
-                                    <th></th>
-                                    <th>Name</th>
-                                    <th>Time</th>
-                                    <th>Calories</th>
+                                <thead>
+                                    <tr className="text-xl">
+                                        <th></th>
+                                        <th className="px-2 py-2">Name</th>
+                                        <th className="px-2 py-2">Time</th>
+                                        <th className="px-2 py-2">Calories</th>
+                                        <th className="px-2 py-2">Actions</th>
+                                    </tr>
                                 </thead>
-                                {/* Table Body */}
+
                                 <tbody>
                                     {
                                         handelRecipes.length > 0 ? (
                                             handelRecipes.map((info, idx) => (
-                                                <tr key={idx}>
-                                                    <td>{idx}</td>
-                                                    <td>{info.recipe_name}</td>  {/* Recipe Name */}
-                                                    <td>{info.recipe_time}</td>        {/* Cooking Time */}
-                                                    <td>{info.recipe_calories}</td>    {/* Calories */}
+                                                <tr key={idx} className="text-lg font-semibold">
+                                                    <td>{idx + 1}</td>
+                                                    <td className="text-2xl text-bold">{info.recipe_name}</td>
+                                                    <td>{info.recipe_time}</td>
+                                                    <td>{info.recipe_calories}</td>
                                                     <td>
-                                                        <button className="btn btn-accent">Preparing</button>
+                                                        <button
+                                                            onClick={() => handelPreparingBtn(info)}
+                                                            className="btn btn-accent">
+                                                            Preparing
+                                                        </button>
                                                     </td>
                                                 </tr>
                                             ))
                                         ) : (
                                             <tr>
-                                                <td colSpan="4" className="text-center">No recipes added yet</td>
+                                                <td colSpan="5" className="text-center text-xl">
+                                                    No recipes added yet
+                                                </td>
                                             </tr>
                                         )
                                     }
                                 </tbody>
                             </table>
                         </div>
-
+                        <div>
+                            <h3 className="border-b-2 pb-3 text-3xl font-bold text-center">
+                                Currently Cooking: {handelPreparing.length}
+                            </h3>
+                        </div>
                     </div>
                 </div>
             </div>
+            <ToastContainer />
         </div>
     );
 };
